0a1b62e51d5db766a73e41194c784572ec4bb8c9
echoutopia <hero-blood@qq.com>
2016-06-26 22:14:32 +0800

    sdf

--- MySQLQuery.py
+++ MySQLQuery.py
@@ -9,13 +9,20 @@ class MySQLQuery:
         self.cusror = self.connection.get_cursor()
         self.logger = Mylogger().logger
 
-    def get_table(self):
+    def get_table(self,timestamp,_type = "day"):
 
-        date_str = time.strftime("%Y%m%d", time.localtime()) 
+        timestamp = timestamp or time.localtime()
+        if _type == "day":
+            _format = "%Y%m%d"
+        elif _type == "month":
+            _format = "%Y%m"
+        elif _type == "year":
+            _format = "%Y"
+        date_str = time.strftime(_format, timestamp)
         return "web_analyse_%s" % date_str
 
 
-    def create_table(self):
+    def create_table(self,timestamp):
 
         sql = '''
         CREATE TABLE `%s` (
@@ -27,7 +34,7 @@ class MySQLQuery:
           PRIMARY KEY (`time,user_id`),
           KEY `analyse_user` (`user_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-        ''' % self.get_table()
+        ''' % self.get_table(timestamp)
         try:
             self.cursor.execute(sql)
             self.cursor.commit()
@@ -40,12 +47,19 @@ class MySQLQuery:
 
 
 
-    def insert_db(self,table,user_id,ip,time,total_time,visit_count):
+    def insert_db(self,timestamp,user_id,ip,time,total_time,visit_count):
 
+        table = self.get_table(timestamp)
         sql = '''
             insert into %s ('user_id','ip','time','total_time','visit_count') values(%s,%s,%s,%s,%s)
         '''
         self.cursor.execute(sql,(table,user_id,ip,time,total_time,visit_count))
         self.cursor.commit()
+
+    def select(self,timestamp,sql,params):
+        table = self.get_table(timestamp)
+        params = params.insert(0,table)
+        result = self.cursor.execute(sql,params)
+        return self.cursor.fetchmany(result)
 
 
---
+++ my_redis.py
@@ -0,0 +1,20 @@
+import redis
+from common import singleton
+
+@singleton
+class MyRedis:
+    def __init__(self,host="127.0.0.1",port=6379,db=0):
+        self.pool = redis.ConnectionPool(host=host,port=port,db=db)
+        self.redis = redis.Redis(connection_pool=pool)
+
+    def get_table(self,user,ip,timestamp,_type="day"):
+
+        timestamp = timestamp or time.localtime()
+        if _type == "day":
+            _format = "%Y%m%d"
+        elif _type == "month":
+            _format = "%Y%m"
+        elif _type == "year":
+            _format = "%Y"
+        date_str = time.strftime(_format, timestamp)
+        return "%s:%s:%s" % (date_str,user,ip)
